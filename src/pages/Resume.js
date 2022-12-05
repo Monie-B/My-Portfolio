979a75a49d267281baa1d77c677e998428195cb8
monie-B <mbullock091@icloud.com>
2022-12-04 22:26:37 -0500

    updated Resume page, fixed icon issue

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -8,21 +8,28 @@ const Resume = () => {
     <>
       <header>
         <h1>
-          Review My Resume
+          Let's Get in Contact
         </h1>
       </header>
       <NavBar />
       <div className='resume-bar'>
-        <div>
-          <img src='Screenshot_20221202_084640.png' alt='resume icon'></img>
+          <img src='Screenshot_20221202_084640.png' alt='Monica resume document'></img>
         </div>
-        {/* <div className='container'>
-          <img src='linkedin.png' alt='LinkedIn icon to navigate to LinkedIn profile'>
-            LinkedIn Profile
-          </img>
-        </div> */}
+        <div className='side-group'>
+          <div className='sm-container'>
+            <div className='linkedin'>
+              <a href='https://www.linkedin.com/in/monica-bullock-502945248/' target='_blank' rel="noreferrer">
+                <img src='linkedin.png' alt='LinkedIn Icon'></img>Let's Connect
+              </a>
+            </div>
+            <div className='github1'>
+            <a href='https://github.com/Monie-B' target='_blank' rel="noreferrer">
+              <img src='github.png' alt='Github icon'></img>Github Profile
+            </a>
+          </div>
       </div>
-    </>
+    </div>
+  </>
   )
 }
 
