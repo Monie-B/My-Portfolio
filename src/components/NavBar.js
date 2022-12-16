b8d140eb519207e1b873c474eede7a38d6cc03e0
monie-B <mbullock091@icloud.com>
2022-12-16 09:31:11 -0500

    restructured nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,17 +5,18 @@ import '../CSS/navbar.css';
 export default function NavBar (props) {
     return (
         <nav className="sidebar">
-            <h3>{props?.user?.name}</h3>
+            {/* <h3>{props?.user?.name}</h3> */}
+                        {/* Nav bar for main link */}
+                        <div className="sidebar-link">
+                <a href="/">
+                    <img src="home.png" className="homepage-icon" alt="homepage icon"></img>Home</a>
+            </div>
             {/* Nav bar for projects link */}
             <div className="sidebar-link">
                 <a href="/projects">
                     <img src="development.png" className="project-icon" alt="project icon"></img>Projects</a>
             </div>
-            {/* Nav bar for main link */}
-            <div className="sidebar-link">
-                <a href="/">
-                    <img src="home.png" className="homepage-icon" alt="homepage icon"></img>Main</a>
-            </div>
+            {/* Nav bar for contact link */}
             <div className="sidebar-link">
                 <a href="/contact">
                     <img src="resume.png" className="resume-icon" alt="resume icon"></img>Contact Me</a>
